2f6d0b506a5621a2e0e8cfe7d918893f3668b3ad
Mike Foxtech <bavdysmyh@ukr.net>
2022-04-30 23:38:14 +0200

    draft of rerunning

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/foxtech6/realtime-build-go/spier"
 	"log"
 	"os/exec"
@@ -8,23 +9,40 @@ import (
 
 func main() {
 	watcher, err := spier.NewWatcher()
+	runC := make(chan string)
+	go run(runC)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = watcher.Watch("/home/mbavdys/Projects/spyfiles/test")
-	if err != nil {
+	if err = watcher.Watch("/home/mbavdys/Projects/spyfiles/test"); err != nil {
 		log.Fatal(err)
 	}
 
 	for {
 		select {
 		case ev := <-watcher.Event:
-			cmd := exec.Command("go", "build")
-			println(cmd.CombinedOutput())
+			//cmd := exec.Command("go", "build", "-o", "file")
+			//println(cmd.CombinedOutput())
 			log.Println(ev.Mask)
+			runC <- "./file123"
+			//cmd1, _ := exec.Command("./file123").Output()
+			//fmt.Printf("OUTPUT: %s", cmd1)
+			//cmd1.Process.Kill()
+
 		case err := <-watcher.Error:
 			log.Println("error:", err)
+		}
+	}
+}
+
+func run(run <-chan string) {
+	for {
+		select {
+		case <-run:
+			cmd1, _ := exec.Command("./file123").Output()
+			fmt.Printf("OUTPUT: %s", cmd1)
 		}
 	}
 }
